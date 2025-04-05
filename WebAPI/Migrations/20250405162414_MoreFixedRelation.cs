b59ecbe703a06c5f618c4c50554c79f67948ac69
AntoniooBagurr <abagur@algebra.hr>
2025-04-05 19:14:16 +0200

    Popravak relacija i migracija

---
+++ WebAPI/Migrations/20250405162414_MoreFixedRelation.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoreFixedRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId1",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_TableId1",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TableId1",
+                table: "Orders");
+
+            migrationBuilder.Sql("UPDATE \"Orders\" SET \"Status\" = '0' WHERE \"Status\" = 'Pending';");
+            migrationBuilder.Sql("UPDATE \"Orders\" SET \"Status\" = '1' WHERE \"Status\" = 'InProgress';");
+            migrationBuilder.Sql("UPDATE \"Orders\" SET \"Status\" = '2' WHERE \"Status\" = 'Completed';");
+            migrationBuilder.Sql("UPDATE \"Orders\" SET \"Status\" = '3' WHERE \"Status\" = 'Cancelled';");
+            migrationBuilder.Sql("UPDATE \"Orders\" SET \"Status\" = '4' WHERE \"Status\" = 'Paid';");
+
+            migrationBuilder.Sql("ALTER TABLE \"Orders\" ALTER COLUMN \"Status\" TYPE integer USING \"Status\"::integer;");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TableId1",
+                table: "Orders",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_TableId1",
+                table: "Orders",
+                column: "TableId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId1",
+                table: "Orders",
+                column: "TableId1",
+                principalTable: "Tables",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
