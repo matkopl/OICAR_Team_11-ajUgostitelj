8cbaa63950d34a3583128fcbdbbc58ee6c8855fb
AntoniooBagurr <abagur@algebra.hr>
2025-04-14 09:01:28 +0200

    Role crud

---
+++ WebAPI/Controllers/RolesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTOs;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoleDto>>> GetAll()
+        {
+            var roles = await _roleService.GetAllRolesAsync();
+            return Ok(roles);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RoleDto>> GetById(int id)
+        {
+            var role = await _roleService.GetRoleByIdAsync(id);
+            return role != null ? Ok(role) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoleDto>> Create(RoleDto roleDto)
+        {
+            try
+            {
+                var createdRole = await _roleService.CreateRoleAsync(roleDto);
+                return CreatedAtAction(nameof(GetById), new { id = createdRole.Id }, createdRole);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, RoleDto roleDto)
+        {
+            try
+            {
+                await _roleService.UpdateRoleAsync(id, roleDto);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _roleService.DeleteRoleAsync(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebAPI/Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.DTOs;
+
+namespace WebAPI.Services
+{
+    public interface IRoleService
+    {
+        Task<IEnumerable<RoleDto>> GetAllRolesAsync();
+        Task<RoleDto?> GetRoleByIdAsync(int id);
+        Task<RoleDto> CreateRoleAsync(RoleDto roleDto);
+        Task UpdateRoleAsync(int id, RoleDto roleDto);
+        Task DeleteRoleAsync(int id);
+    }
+}
