399ff8b549bb7d8c3309655bd156f66d7ae67169
AntoniooBagurr <abagur@algebra.hr>
2025-04-18 19:55:08 +0200

    Details page

---
+++ WebApp/Pages/Product/Details.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Product
+{
+    public class DetailsModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int? Id { get; set; }  // Ovo će biti null dok ne povežete s bazom
+
+        public void OnGet()
+        {
+            // Za sada ne radimo ništa s ID-om
+            // Kasnije ćete ovdje dohvatiti proizvod iz baze
+        }
+    }
+}
