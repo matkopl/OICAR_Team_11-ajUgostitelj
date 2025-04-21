d9a5f4686098e06b826696425122fabd8e997a24
Matko Gečević <mgecevic@algebra.hr>
2025-04-21 22:52:47 +0200

    products

---
+++ WebApp/DTOs/OrderStatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.DTOs
+{
+    public class OrderStatusDto
+    {
+        public int OrderId { get; set; }
+        public string Status { get; set; } 
+        public string? Notes { get; set; } 
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
 using WebAPI.Models;
+using WebApp.ApiClients;
+using WebAPI.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("conStr")));
@@ -14,6 +18,22 @@ builder.Services.AddHttpClient("ProductsAPI", client =>
 {
     client.BaseAddress = new Uri("https://localhost:5207/"); 
     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
+
+builder.Services.AddHttpClient<ProductApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://oicar-team-11-ajugostitelj-11.onrender.com/api/");
+    client.DefaultRequestHeaders
+          .Accept
+          .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    // you can also set timeouts, add Polly policies, etc., here
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
 });
 
 //builder.Services.AddAutoMapper(typeof(Program));
@@ -25,6 +45,15 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Product}/{action=Index}/{id?}"
+);
+
+app.UseSession();
+
+
 app.UseStaticFiles();
 
 app.UseRouting();
