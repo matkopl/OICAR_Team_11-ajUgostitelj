f007f5a986f73f564f198d385fd1508fc7fe6885
Matko Gečević <mgecevic@algebra.hr>
2025-05-04 19:39:19 +0200

    fix

--- WebApp/DTOs/OrderStatusDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAPI.DTOs
-{
-    public class OrderStatusDto
-    {
-        public int OrderId { get; set; }
-        public string Status { get; set; } 
-        public string? Notes { get; set; } 
-    }
-}
--- WebApp/DTOs/TableQueryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAPI.DTOs
-{
-    public class TableQueryDto
-    {
-        public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 5;
-        public bool? IsOccupied { get; set; }
-    }
-}
